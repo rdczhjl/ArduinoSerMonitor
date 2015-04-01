d6b62ed9a63ae247b9c0af6a68492b90f7d2b291
rdczhjl <zhang_jialu@hotmail.com>
2015-04-01 16:47:04 +0800

    draft

---
+++ PythonClient/SerialMonitor.py
@@ -0,0 +1,17 @@
+'''
+Created on Apr 1, 2015
+
+@author: rdczhjl
+'''
+
+class SerialMonitor(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
