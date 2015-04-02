447de8150f4ade45ffb2fc9a5dd575537826deab
rdczhjl <zhang_jialu@hotmail.com>
2015-04-02 18:38:15 +0800

    introduce standard io

--- PythonClient/SerialMonitor.py
+++ PythonClient/SerialMonitor.py
@@ -7,26 +7,66 @@ It is the Client towards Arduino
 import sys
 import serial
 
-
-
-''' ==============main ======================== '''
-
-welcomeStr= "Hello, Welcome to the Arduino Serial Monitor"
-print (welcomeStr)
-
-yourCmd=input("Please input your command")
-print ("Your Command is"+yourCmd)
+global running
+global monitor
 
 
 
+
+# ==============class definition ======================== 
 class SerialMonitor(object):
     '''
     classdocs
     '''
 
 
-    def __init__(self, params):
+
+#Constructor
+    def __init__(self, param):
+        #setup the serial connection
         '''
-        Constructor
+        classdocs
         '''
         
+    def func1(self):
+        print ("here is func1 in SerialMonitor")
+        
+    def func2(self):
+        print ("here is func2 in SerialMonitor")
+        
+
+# ==============define the func ==================
+def byebye():
+        print ("byebye , leaving ")
+        running=False
+        exit(0)
+
+
+def extfunc1():
+        print ("here is extfunc1")
+
+# ==============define the dictionary ==================
+theCmdDict={'bye':byebye,'extfunc1':extfunc1,'func1':SerialMonitor.func1}
+
+
+# ==============main ======================== 
+
+welcomeStr= "==========Hello, Welcome to the Arduino Serial Monitor=========="
+print (welcomeStr)
+
+
+running=True
+monitor=SerialMonitor("dummy")
+
+#lookup dictionary to understand the cmd
+while running:
+        #get the intension 
+        theIntension=raw_input("Please input your command:")
+        print ("Your Command is "+theIntension)
+        #translate the intension to the command
+       
+        func=theCmdDict[theIntension]
+        func();
+
+          
+exit (0)    
